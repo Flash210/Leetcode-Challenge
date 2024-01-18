a1836ebf8436986e9f1b3400df166efaf8864d51
ReversedFlash <houcem.eddine.mosbah@gmail.com>
2024-01-18 12:41:05 +0100

    sol2

--- Welcome/roman to integer/solution.dart
+++ Welcome/roman to integer/solution.dart
@@ -4,12 +4,16 @@
 void main(){
 
 
-  print(romanToInt("CDXCVII"));
+  print(romanToIntOne("CDXC")); 
+  // TC(n) where n is the length of the string 
+  // SC(n ) because we use a data structure to store roman letters 
+print(romanToIntgerTwo("CDXC"));
+// SC(1) because we use only two constants 
+  
 }
 
 
-int romanToInt(String s ){
-
+int romanToIntOne(String s ){
 
 int value = 0;
 
@@ -32,6 +36,33 @@ for ( int i =0;i<s.length;i++){
 
 
 }
+
+  return value ; 
+}
+
+
+// ************************************* solution 2 
+int romanToIntgerTwo(String s){
+  int value =0;
+  int num =0;
+
+   for (String  c in s.split('').reversed) {
+    switch(c ){
+ case 'I': num = 1; break;
+                case 'V': num = 5; break;
+                case 'X': num = 10; break;
+                case 'L': num = 50; break;
+                case 'C': num = 100; break;
+                case 'D': num = 500; break;
+                case 'M': num = 1000; break;
+
+    }
+    if ( value >4*num )
+    value-=num;
+    else 
+    value+=num;
+    
+  }
 
   return value ; 
 }
